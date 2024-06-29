763c53c598bcae5567c79c35d92b5b012984fe95
yuzalsif <liteit68@gmail.com>
2024-06-29 16:21:32 +0300

    chore: Add google_fonts package

--- packages/features/home/lib/src/home_screen.dart
+++ packages/features/home/lib/src/home_screen.dart
@@ -10,6 +10,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Color(0xFFE5E5E5),
+      body: Column(
+
+      )
+    );
   }
 }
+
+class PatientNeedContainer extends StatelessWidget {
+  final Color needColor;
+  final String needIssue;
+  final String wardName;
+  final String roomNumber;
+
+  const PatientNeedContainer({super.key,
+    required this.needColor,
+    required this.needIssue,
+    required this.wardName,
+    required this.roomNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: needColor,
+      child: Column(
+        children: [
+          Text(needIssue),
+          Text(wardName),
+          Text(roomNumber),
+        ],
+      ),
+    );
+  }
+}
+
