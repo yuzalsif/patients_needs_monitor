5cdfad2ba563b016e5b24bf7a9a1e88fd7b440cd
yuzalsif <liteit68@gmail.com>
2024-06-29 16:14:11 +0300

    Add home screen from home package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:patients_needs_monitor/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PatientsNeedsMonitor());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
